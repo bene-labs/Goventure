57ba591fa47419bc1a97cdbc36e2da977236248a
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-05-06 10:14:12 +0200

    update file structure. fix bugs

---
+++ addons/goventure/visual_scripting/Cable.gd
@@ -0,0 +1,143 @@
+class_name Cable extends Control
+
+@onready var line : Line2D = $Outline/Line2D
+@onready var outline : Line2D = $Outline
+@onready var collision_shape : CollisionPolygon2D = $Area2D/CollisionShape
+var collision_rect : Rect2
+
+var undefined_color = Color.RED
+var off_color = Color("545151")
+var on_color = Color("241ec9")
+var hover_color = Color.CORNFLOWER_BLUE
+var side_offset = 5
+
+var connected_output = null
+var connected_input = null
+var is_hovered = false
+
+var has_started_from_input = false
+
+func _ready():
+	CursorCollision.register(self)
+
+func update_loose_point(position) -> bool:
+	if connected_output == null:
+		set_start_point(position)
+	elif connected_input == null:
+		set_end_point(position)
+	else:
+		return false
+	return true
+
+func set_point(node):
+	set_start_point(node.global_position) if node is Output else set_end_point(node.global_position)
+
+func adjust_color(state):
+	match state.value:
+		TriState.State.TRUE:
+			line.default_color = on_color
+		TriState.State.FALSE:
+			line.default_color = off_color
+		_:
+			line.default_color = undefined_color
+
+func set_start_point(point : Vector2):
+	line.points[0] = point
+	outline.points[0] = point
+	
+	calc_collision(outline.points[0], outline.points[-1])
+
+func get_start_point():
+	return line.points[0]
+
+func set_end_point(point : Vector2):
+	line.points[-1] = point
+	outline.points[-1] = point
+	calc_collision(outline.points[-2], outline.points[-1])
+
+func calc_collision(start, end):
+	collision_shape.polygon[0] = start + start.direction_to(end) * side_offset + \
+		start.direction_to(end).rotated(deg_to_rad(-90)).normalized() * outline.width / 2
+	collision_shape.polygon[1] = start + start.direction_to(end) * side_offset + \
+		start.direction_to(end).rotated(deg_to_rad(90)).normalized() * outline.width / 2
+	collision_shape.polygon[2] = end + end.direction_to(start) * side_offset + \
+		start.direction_to(end).rotated(deg_to_rad(90)).normalized() * outline.width / 2
+	collision_shape.polygon[3] = end + end.direction_to(start) * side_offset + \
+		start.direction_to(end).rotated(deg_to_rad(-90)).normalized() * outline.width / 2
+
+func is_point_inside(point) -> bool:
+	return Geometry2D.is_point_in_polygon(point, collision_shape.polygon)
+
+func connect_to(connection):
+	if connection is Output:
+		connect_output(connection)
+	else:
+		connect_input(connection)
+
+func connect_input(input):
+	if connected_output == null:
+		has_started_from_input = true
+	connected_input = input
+	set_end_point(input.global_position)
+	input.position_changed.connect(_on_input_position_changed)
+	
+func connect_output(output):
+	if connected_input == null:
+		has_started_from_input = false
+	connected_output = output
+	set_start_point(output.global_position)
+	output.position_changed.connect(_on_output_position_changed)
+
+func _on_input_position_changed(new_pos):
+	set_end_point(new_pos)
+
+func _on_output_position_changed(new_pos):
+	set_start_point(new_pos)
+
+func get_end_point():
+	return line.points[1]
+
+@warning_ignore("native_method_override")
+func set_z_index(new_index):
+	outline.z_index = new_index
+#	if connected_input != null:
+#		connected_input.set_z_index(new_index + 1)
+#	if connected_output != null:
+#		connected_output.set_z_index(new_index + 1)
+
+@warning_ignore("native_method_override")
+func get_z_index():
+	return outline.z_index
+
+func _on_mouse_entered():
+	outline.default_color = hover_color
+	is_hovered = true
+
+func _on_mouse_exited():
+	outline.default_color = Color.BLACK
+	is_hovered = false
+
+func _input(event):
+	if not is_hovered:
+		return
+	if Input.is_action_just_pressed("destroy"):
+		queue_free()
+
+func _exit_tree():
+	CursorCollision.unregister(self)
+	if weakref(connected_output).get_ref():
+		if connected_output is InputConnection:
+			if connected_output.connected_cable != null:
+				connected_output.connected_cable.queue_free()
+			connected_output.queue_free()
+		else:
+			connected_output.remove_cable(self)
+	if !weakref(connected_input).get_ref() or ("connected_cable" in connected_input and connected_input.connected_cable != self):
+		return
+	if connected_input is Output:
+		connected_input.remove_cable(self)
+		connected_input.delete_all_cables()
+		connected_input.queue_free()
+		return
+	connected_input.connected_cable = null
+	connected_input.set_state(TriState.State.UNDEFINED)
---
+++ addons/goventure/visual_scripting/Gates.gd
@@ -0,0 +1,76 @@
+class_name Gates
+extends Control
+
+signal drag_toggled(new_state)
+
+@export var cables : Control
+@onready var button_rect = $ColorRect
+
+var buttons = []
+var gates : Array#get_tree().get_nodes_in_group("Gate")
+
+
+var dragged_gate = null
+var drag_offset = Vector2.ZERO
+var is_mouse_movement = false
+
+func _ready():
+	for gate in gates:
+		gate.clicked.connect(_on_gate_clicked)
+		gate.released.connect(_on_gate_released)
+		gate.destroyed.connect(_on_gate_destroyed)
+	call_deferred("apply_gate_z_index")
+	#for button in button_rect.get_children():
+		#buttons.append(button)
+		#button.gate_spawned.connect(_on_gate_spawned)
+
+func put_gate_in_front(gate):
+	gates.erase(gate)
+	gates.push_front(gate)
+	apply_gate_z_index()
+	CursorCollision.update_order()
+
+func apply_gate_z_index():
+	var z_index = gates.size() * 3
+	for gate in gates:
+		gate.set_z_index(z_index)
+		z_index -= 3
+
+func _on_gate_clicked(gate, offset: Vector2):
+	put_gate_in_front(gate)
+	dragged_gate = gate
+	drag_offset = offset
+	gate.set_dragged()
+	emit_signal("drag_toggled", true)
+
+func _on_gate_released(gate):
+	gate.set_undragged()
+	dragged_gate = null
+	emit_signal("drag_toggled", false)
+
+func _on_gate_destroyed(gate):
+	gates.erase(gate)
+
+func _process(delta):
+	if dragged_gate == null or not is_mouse_movement:
+		return
+	dragged_gate.update_position(dragged_gate.get_global_mouse_position() + drag_offset)
+
+func _input(event):
+	is_mouse_movement = true if event is InputEventMouseMotion and event.relative else false
+
+func update_button_dimensions():
+	for i in range(buttons.size()):
+		buttons[i].size = Vector2(button_rect.size.x * 0.9,  button_rect.size.y / (buttons.size() + 1.25) )
+		buttons[i].global_position = Vector2(button_rect.size.x * 0.05,  button_rect.size.y / buttons.size() * i)
+
+func _on_gate_spawned(new_gate):
+	gates.append(new_gate)
+	new_gate.clicked.connect(_on_gate_clicked)
+	new_gate.released.connect(_on_gate_released)
+	new_gate.destroyed.connect(_on_gate_destroyed)
+	for input in new_gate.inputs:
+		cables.register_input(input)
+	for output in new_gate.outputs:
+		cables.register_output(output)
+	
