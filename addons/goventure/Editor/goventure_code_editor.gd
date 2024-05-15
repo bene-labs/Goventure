ba17bbd4e3fe750bd8d4c4337e9e6a3c8769ae80
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-05-15 14:35:25 +0200

    partially fix CodeEdit highlighting

--- addons/goventure/Editor/goventure_code_editor.gd
+++ addons/goventure/Editor/goventure_code_editor.gd
@@ -4,9 +4,29 @@ extends CodeEdit
 var current_file : FileAccess
 
 
-func _input(event):
-	if event.is_action_pressed("save"):
+func _enter_tree():
+	var code_highlighting = CodeHighlighter.new()
+	code_highlighting.add_keyword_color("with", Color.CORAL)
+	code_highlighting.add_keyword_color("say", Color.PALE_VIOLET_RED)
+	code_highlighting.add_color_region('\"', '\"', Color.BISQUE)
+	code_highlighting.symbol_color = Color.WHITE
+	code_highlighting.member_variable_color = Color.WHITE
+	code_highlighting.number_color = Color.WHITE
+	code_highlighting.function_color = Color.WHITE
+	syntax_highlighter = code_highlighting
+	
+	#for action in Goventure.actions:
+		#code_highlighting.add_keyword_color(action, Color.SKY_BLUE)
+	#for interactible in Goventure.interactibles:
+		#code_highlighting.add_keyword_color(interactible, Color.SKY_BLUE)
+		
+	
+
+
+func _notification(what):
+	if what == NOTIFICATION_EDITOR_PRE_SAVE:
 		save_current_file()
+
 
 func save_current_file():
 	if current_file == null:
