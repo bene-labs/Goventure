af02989dc689646c7c58d5f1771422e8188d2f19
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-06-01 18:56:56 +0200

    cleanup project

--- addons/goventure/scripts/DialoguePath.gd
+++
@@ -1,5 +0,0 @@
-class_name GoventureDialoguePath
-extends Resource
-
-@export var is_looping := false
-@export var path : Array[GoventureDialogueQA]
--- addons/goventure/scripts/DialogueQA.gd
+++
@@ -1,8 +0,0 @@
-class_name GoventureDialogueQA
-
-extends Resource
-
-#@export var required_key := ""
-#@export var skip_on_key := ""
-@export_multiline var question := "" 
-@export_multiline var answer := "" 
